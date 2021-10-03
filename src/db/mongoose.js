f30d8677b85de6811c9758f5d1689c8d4c89980d
grimreaperLinux <20bds007@iiitdwd.ac.in>
2021-10-03 22:15:56 +0530

    Adding some models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,57 +1,57 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
+// const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        trim: true
-    },
-    age:{
-        type: Number,
-        default: 0,
-        validate(value){
-            if (value < 0){
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        validate(value) {
-            if(!validator.isEmail(value))
-            throw new Error('Email is invalid')
-        }
-    },
-    password: {
-        type:String,
-        required:true,
-        minlenght: 7,
-        trim: true,
-        validate(value){
-            if(value.toLowerCase().includes('Password')){
-                throw new error('Password cannot contain "Password"')
-            }
-        }
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//         trim: true
+//     },
+//     age:{
+//         type: Number,
+//         default: 0,
+//         validate(value){
+//             if (value < 0){
+//                 throw new Error('Age must be a positive number')
+//             }
+//         }
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         validate(value) {
+//             if(!validator.isEmail(value))
+//             throw new Error('Email is invalid')
+//         }
+//     },
+//     password: {
+//         type:String,
+//         required:true,
+//         minlenght: 7,
+//         trim: true,
+//         validate(value){
+//             if(value.toLowerCase().includes('Password')){
+//                 throw new error('Password cannot contain "Password"')
+//             }
+//         }
         
-    } 
-})
+//     } 
+// })
 
-const me = new User({
-    name:'Aniket Raj',
-    age: -20,
-    email: 'Mikeapp@gmail.com',
-    password:'        re32     '
-})
+// const me = new User({
+//     name:'Aniket Raj',
+//     age: -20,
+//     email: 'Mikeapp@gmail.com',
+//     password:'        re32     '
+// })
 
-me.save().then((me) => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// me.save().then((me) => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
 
 // const Task = mongoose.model('Task', {
 //     description: {
---
+++ src/models/tasks.js
@@ -0,0 +1,15 @@
+const validator = require('validator')
+const mongoose = require('mongoose')
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true
+    },
+    completed:{
+        type: Boolean,
+        default: false
+    } 
+})
+
+module.exports = Task
