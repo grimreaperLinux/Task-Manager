21ac51fb3f9dded4481b2b97851a88b95ea38fca
grimreaperLinux <20bds007@iiitdwd.ac.in>
2021-10-06 23:28:07 +0530

    Changing all routes to async-await

--- src/index.js
+++ src/index.js
@@ -25,58 +25,130 @@ app.post('/users', async (req,res) => {
     // })
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+    
+    try{
+        const users = await User.find({})
         res.send(users)
-    }).catch(() => {
+    }
+    catch(e){
         res.status(500).send()
-    })
+    }
+    
+    // User.find({}).then((users) => {
+    //     res.send(users)
+    // }).catch(() => {
+    //     res.status(500).send()
+    // })
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
 
-    User.findById(_id).then((user) => {
+    try{
+        const user = await User.findById(_id)
+
+        if(!user)
+        {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e){
+        res.status(500).send( )
+    }
+    // User.findById(_id).then((user) => {
+    //     if (!user) {
+    //         return res.status(404).send()
+    //     }
+
+    //     res.send(user)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
+})
+
+app.post('/tasks',async (req,res) => {
+    const task = new Task(req.body)
+
+    try{
+        await task.save()
+        res.status(201).send(task)
+        }
+        catch(e){
+            res.status(400).send(e)
+        }
+
+    // task.save().then((task) => {
+    //     res.status(201).send(task)
+    // }).catch((error) => {
+    //     res.status(400).send(error)
+    // })
+})
+
+app.get('/tasks', async (req, res) => {
+    
+    try{
+        const tasks = await Task.find({})
+        res.send(tasks)
+    }
+    catch(e){
+        res.status(500).send()
+    }
+    
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
+})
+
+app.get('/tasks/:id',async (req, res) => {
+    const _id = req.params.id
+
+    try{
+        const task = await Task.findById(_id)
+
+        if(!task)
+        {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e){
+        res.status(500).send()
+    }
+
+    // Task.findById(_id).then((task) => {
+    //     if (!task) {
+    //         return res.status(404).send()
+    //     }
+
+    //     res.send(task)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
+})
+
+
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    
         if (!user) {
             return res.status(404).send()
         }
 
         res.send(user)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.post('/tasks', (req,res) => {
-    const task = new Task(req.body)
-
-    task.save().then((task) => {
-        res.status(201).send(task)
-    }).catch((error) => {
-        res.status(400).send(error)
-    })
-})
-
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.get('/tasks/:id', (req, res) => {
-    const _id = req.params.id
-
-    Task.findById(_id).then((task) => {
-        if (!task) {
-            return res.status(404).send()
-        }
-
-        res.send(task)
-    }).catch((e) => {
-        res.status(500).send()
-    })
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
 app.listen(port, () => {
